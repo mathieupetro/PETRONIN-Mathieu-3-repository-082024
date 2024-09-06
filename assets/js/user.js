be07363bbea9aacf57c01a2d9b77884959b81693
PETRONIN <mathieu.petronin@laposte.net>
2024-09-06 22:41:51 +0200

    Mise à jour du fichier user.js et de login.html

--- assets/js/user.js
+++ assets/js/user.js
@@ -6,6 +6,15 @@ const form = { //Déclaration de l'objet//
 
 let button = form.submit.addEventListener("click", (e) => { //Ajout d'un écouteur d'événement sur le bouton de soumission//
     e.preventDefault();
+
+    const errorMessageDiv = document.getElementById("error-message");
+    errorMessageDiv.textContent = ""; // Réinitialise le message d'erreur à chaque soumission
+
+    if (!form.email.value || !form.password.value) {
+        errorMessageDiv.textContent = "Veuillez remplir les champs email et mot de passe.";
+        return; // Arrête l'exécution si les champs sont vides
+    }
+
     const login = "http://localhost:5678/api/users/login"; //Définition de l'URL de l'API//
 
     fetch(login, { //Envoi de la requête POST à l'API//
@@ -21,18 +30,24 @@ let button = form.submit.addEventListener("click", (e) => { //Ajout d'un écoute
     })
         .then((response) => response.json()) //Traitement de la réponse de l'API//
         .then((data) => {
-            var token = data.token;
-            var isLoggedIn = true;
-            if (data.message === "user not found") {
-                alert("Erreur dans l’identifiant ou le mot de passe");
+            const errorMessageDiv = document.getElementById("error-message"); // Sélectionne l'élément pour afficher l'erreur
+            if (data.error) {
+                if (data.error === "mot de passe incorrect") {
+                    errorMessageDiv.textContent = "Mot de passe incorrect. Veuillez réessayer.";
+                } else {
+                    errorMessageDiv.textContent = "Erreur dans l’identifiant ou le mot de passe.";
+                }
             }
             else {
+                const token = data.token;
+                const isLoggedIn = true;
                 sessionStorage.setItem("isLoggedIn", isLoggedIn);
                 sessionStorage.setItem("token", token);
                 location.assign("index.html");
             }
-            console.log(token, isLoggedIn);
         })
+
+
         .catch((err) => { //Gestion des erreurs//
             console.log(err);
         });
